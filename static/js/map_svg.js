d7e88acffb5372aad960d9d6f23b31fbcdb0ef00
Shashank Nerurkar <shash.nerurkar@gmail.com>
2021-03-28 10:01:00 +0530

    dwqefebgrs dwqefwbg

--- static/js/map_svg.js
+++ static/js/map_svg.js
@@ -1,6 +1,11 @@
+func lokAllOnHovers() {
+    var world = {'AN':1, 'AP':17, 'AR':2, 'AS':14, 'BR':40, 'CH':1, 'CT':11, 'DD':1, 'DL':7, 'DN':1, 'GA':2, 'GJ':26, 'HP':4, 'HR':13, 'JH':14, 'JK':5, 
+                 'KA':28, 'KL':20, 'LD':1, 'MH':48, 'ML':2, 'MN':2, 'MP':29, 'MZ':1, 'NL':1, 'OR':21, 'PB':10, 'PY':1, 'RJ':25, 'SK':1, 'TG':25, 'TN':39, 'TR':2, 'UP':80, 'UT':5, 'WB':42};
+    object.onmouseover = function(){myScript};
+}
 
-func allOnHovers() {
-    var world = {'GJ': 5, 'AN', 'AP', 'AR', 'AS', 'BR', 'CH', 'CT', 'DD', 'DL', 'DN', 'GA', 'GJ', 'HP', 'HR', 'JH', 'JK', 
-                 'KA', 'KL', 'LD', 'MH', 'ML', 'MN', 'MP', 'MZ', 'NL', 'OR', 'PB', 'PY', 'RJ', 'SK', 'TG', 'TN', 'TR', 'UP', 'UT', 'WB'};
+func rajyaAllOnHovers() {
+    var world = {'AN':0, 'AP':7, 'AR':1, 'AS':7, 'BR':16, 'CH':0, 'CT':5, 'DD':0, 'DL':3, 'DN':0, 'GA':1, 'GJ':11, 'HP':3, 'HR':7, 'JH':6, 'JK':4, 
+                 'KA':12, 'KL':9, 'LD':0, 'MH':19, 'ML':1, 'MN':1, 'MP':11, 'MZ':1, 'NL':1, 'OR':10, 'PB':5, 'PY':1, 'RJ':10, 'SK':1, 'TG':11, 'TN':18, 'TR':1, 'UP':31, 'UT':3, 'WB':16};
     object.onmouseover = function(){myScript};
 }
