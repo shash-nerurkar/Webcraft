8465690e86a80e6728545748d831d247d2dfd0d0
Shashank Nerurkar <shash.nerurkar@gmail.com>
2021-03-28 09:33:13 +0530

    index.x

---
+++ static/js/map_svg.js
@@ -0,0 +1,6 @@
+
+func allOnHovers() {
+    var world = {'GJ': 5, 'AN', 'AP', 'AR', 'AS', 'BR', 'CH', 'CT', 'DD', 'DL', 'DN', 'GA', 'GJ', 'HP', 'HR', 'JH', 'JK', 
+                 'KA', 'KL', 'LD', 'MH', 'ML', 'MN', 'MP', 'MZ', 'NL', 'OR', 'PB', 'PY', 'RJ', 'SK', 'TG', 'TN', 'TR', 'UP', 'UT', 'WB'};
+    object.onmouseover = function(){myScript};
+}
